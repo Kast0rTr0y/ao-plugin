f2a7743cf7d3e4da0a8b615173dc8d15a6c92450
Samuel Le Berrigaud <sleberrigaud@atlassian.com>
2010-09-28 09:21:21 +0000

    - Simplyfied HexMd5Digester

--- bundle/src/main/java/com/atlassian/activeobjects/util/HexMd5Digester.java
+++ bundle/src/main/java/com/atlassian/activeobjects/util/HexMd5Digester.java
@@ -1,7 +1,7 @@
 package com.atlassian.activeobjects.util;
 
-import static java.lang.Math.max;
 import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
+import static org.apache.commons.lang.StringUtils.right;
 
 /**
  * <p>A digester that creates an hexadecimal representation of the md5 sum of the String it applies to.</p>
@@ -16,11 +16,6 @@ public final class HexMd5Digester implements Digester
 
     public String digest(String s, int n)
     {
-        return getLastNCharacters(digest(s), n);
-    }
-
-    String getLastNCharacters(String digested, int n)
-    {
-        return digested.substring(max(0, digested.length() - n));
+        return right(digest(s), n);
     }
 }
