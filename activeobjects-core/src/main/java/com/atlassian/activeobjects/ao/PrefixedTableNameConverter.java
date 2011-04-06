e69a8c5c6df9a7910407f39ad897b9743d534cf9
Samuel Le Berrigaud <sleberrigaud@atlassian.com>
2011-04-06 08:52:52 +0000

    AO-102: Enable the use of the @Table annotation in Active Objects, making sure that prefix and other rules are still applied when using the annotation. All is explained in the com.atlassian.activeobjects.ao.ActiveObjectsTableNameConverter javadocs.

--- activeobjects-core/src/main/java/com/atlassian/activeobjects/ao/PrefixedTableNameConverter.java
+++ activeobjects-core/src/main/java/com/atlassian/activeobjects/ao/PrefixedTableNameConverter.java
@@ -3,24 +3,18 @@ package com.atlassian.activeobjects.ao;
 import com.atlassian.activeobjects.internal.Prefix;
 import net.java.ao.RawEntity;
 import net.java.ao.schema.TableNameConverter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static com.google.common.base.Preconditions.*;
 
 /**
- * A {@link TableNameConverter table name converter} that will prepend the given {@link com.atlassian.activeobjects.internal.Prefix} to table names.
- * It uses a {@link TableNameConverter delegate table name converter} for the <em>general</em> conversion strategy.
+ * <p>A {@link TableNameConverter table name converter} that will prepend the given {@link Prefix} to table names.</p>
+ * <p>It uses a {@link TableNameConverter delegate table name converter} for the <em>general</em> conversion strategy.</p>
  */
 final class PrefixedTableNameConverter implements TableNameConverter
 {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
     private final Prefix prefix;
 
-    /**
-     * The table name converter we delegate the real conversion to
-     */
+    /** The table name converter we delegate the real conversion to */
     private final TableNameConverter delegate;
 
     public PrefixedTableNameConverter(Prefix prefix, TableNameConverter delegate)
@@ -31,8 +25,6 @@ final class PrefixedTableNameConverter implements TableNameConverter
 
     public String getName(Class<? extends RawEntity<?>> clazz)
     {
-        final String tableName = prefix.prepend(delegate.getName(clazz));
-        logger.debug("Table name for class <{}> is <{}>", clazz.getName(), tableName);
-        return tableName;
+        return prefix.prepend(delegate.getName(clazz));
     }
 }
