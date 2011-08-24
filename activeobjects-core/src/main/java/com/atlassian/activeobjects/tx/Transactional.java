cd81e4fc61f0893b6e7e26d78843b698d8d230ad
Samuel Le Berrigaud <sleberrigaud@atlassian.com>
2011-08-24 12:47:54 +0000

    AO-167: Fixing documentation of @Transactional annotation.

--- activeobjects-core/src/main/java/com/atlassian/activeobjects/tx/Transactional.java
+++ activeobjects-core/src/main/java/com/atlassian/activeobjects/tx/Transactional.java
@@ -1,5 +1,8 @@
 package com.atlassian.activeobjects.tx;
 
+import com.atlassian.activeobjects.external.TransactionalAnnotationProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
@@ -10,11 +13,13 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * <p>Annotated methods of an interface or the type itself will make those methods run within a transaction
+ * <p>Annotating methods of an interface with this annotation will make those methods run within a transaction
  * provided by the host application.</p>
  * <p><strong>Note</strong> that in order for this annotation to be processed, one must declare the
- * {@link com.atlassian.activeobjects.external.TransactionalAnnotationProcessor} as a component within their plugin.</p>
- * @see com.atlassian.activeobjects.external.TransactionalAnnotationProcessor
+ * {@link TransactionalAnnotationProcessor} as a component within their plugin.
+ * This processor is a {@link BeanPostProcessor} which will only be able to handle classes instanciated as a
+ * <a href="http://confluence.atlassian.com/display/PLUGINFRAMEWORK/Component+Plugin+Module">components</a>.</p>
+ * @see TransactionalAnnotationProcessor
  */
 @Retention(RUNTIME)
 @Target({TYPE, METHOD})
