1c926dc9b6f807dcdc46de168f15719034ea17d2
Samuel Le Berrigaud <sleberrigaud@atlassian.com>
2011-08-24 13:20:32 +0000

    AO-168: Fixing exception being thrown from @Transactional methods.

--- activeobjects-plugin/src/test/java/com/atlassian/activeobjects/external/TransactionalAnnotationProcessorTest.java
+++ activeobjects-plugin/src/test/java/com/atlassian/activeobjects/external/TransactionalAnnotationProcessorTest.java
@@ -1,15 +1,20 @@
 package com.atlassian.activeobjects.external;
 
 import com.atlassian.activeobjects.tx.Transactional;
+import com.atlassian.sal.api.transaction.TransactionCallback;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
 import static junit.framework.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.when;
 
 /**
  * Testing {@link com.atlassian.activeobjects.external.TransactionalAnnotationProcessor}
@@ -63,6 +68,38 @@ public class TransactionalAnnotationProcessorTest
         };
         final Object proxy = transactionalAnnotationProcessor.postProcessAfterInitialization(o, "a-bean-name");
         assertFalse(o == proxy);
+    }
+
+    @Test
+    public void throwingExceptionInTransactionalMethodActuallyThrowsSameException()
+    {
+        when(ao.executeInTransaction(Matchers.<TransactionCallback<Object>>any())).thenAnswer(new Answer()
+        {
+            public Object answer(InvocationOnMock invocation)
+            {
+                return ((TransactionCallback) invocation.getArguments()[0]).doInTransaction();
+            }
+        });
+
+        final RuntimeException expectedException = new RuntimeException();
+        final Object o = new AnnotatedMethodInInterface()
+        {
+            public void doSomething()
+            {
+                throw expectedException;
+            }
+        };
+
+        final AnnotatedMethodInInterface proxy = (AnnotatedMethodInInterface) transactionalAnnotationProcessor.postProcessAfterInitialization(o, "a-bean-name");
+        assertFalse(o == proxy);
+        try
+        {
+            proxy.doSomething();
+        }
+        catch (Exception actualException)
+        {
+            assertSame(expectedException, actualException);
+        }
     }
 
     @Transactional
