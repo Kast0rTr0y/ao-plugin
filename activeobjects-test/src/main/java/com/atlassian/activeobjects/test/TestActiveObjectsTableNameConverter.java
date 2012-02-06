cf3789d508eec47f31a06a7a14aed08fb77b8980
Samuel Le Berrigaud <sleberrigaud@atlassian.com>
2012-02-06 13:48:13 +0000

    AO-303: Making sure all calls to String.toLowerCase and String.toUpperCase actually use a default locale. This will prevent the turkish 'i' issue.

--- activeobjects-test/src/main/java/com/atlassian/activeobjects/test/TestActiveObjectsTableNameConverter.java
+++ activeobjects-test/src/main/java/com/atlassian/activeobjects/test/TestActiveObjectsTableNameConverter.java
@@ -3,6 +3,7 @@ package com.atlassian.activeobjects.test;
 import com.atlassian.activeobjects.ao.ActiveObjectsTableNameConverter;
 import com.atlassian.activeobjects.internal.Prefix;
 import net.java.ao.RawEntity;
+import net.java.ao.schema.Case;
 import net.java.ao.schema.TableNameConverter;
 
 public final class TestActiveObjectsTableNameConverter implements TableNameConverter
@@ -20,8 +21,8 @@ public final class TestActiveObjectsTableNameConverter implements TableNameConve
         @Override
         public boolean isStarting(String string, boolean caseSensitive)
         {
-            final String thePrefix = caseSensitive ? prefix : prefix.toLowerCase();
-            final String toCompare = caseSensitive ? string : string.toLowerCase();
+            final String thePrefix = caseSensitive ? prefix : Case.LOWER.apply(prefix);
+            final String toCompare = caseSensitive ? string : Case.LOWER.apply(string);
             return toCompare.startsWith(thePrefix);
         }
     });
