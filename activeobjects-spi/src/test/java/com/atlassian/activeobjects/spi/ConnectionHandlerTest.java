48506a35e73896188eb6ccb0752fb1e6be09a019
Dariusz Kordonski <dkordonski@atlassian.com>
2012-09-17 11:13:52 +1000

    AO-364 cherry picking original fix to the Connectionandler to the development branch

--- activeobjects-spi/src/test/java/com/atlassian/activeobjects/spi/ConnectionHandlerTest.java
+++ activeobjects-spi/src/test/java/com/atlassian/activeobjects/spi/ConnectionHandlerTest.java
@@ -1,15 +1,24 @@
 package com.atlassian.activeobjects.spi;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import java.sql.*;
-import java.util.Map;
-import java.util.Properties;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * <p>Unit tests for {@link ConnectionHandler}.</p>
  */
 public class ConnectionHandlerTest {
+
+
+    @Rule public ExpectedException exception = ExpectedException.none();
 
     /**
      * <p>Test that public methods can be invoked when the delegate is an instance of a private class.</p>
@@ -19,6 +28,26 @@ public class ConnectionHandlerTest {
     @Test
     public void testPrivateDelegate() throws Exception {
         ConnectionHandler.newInstance(PrivateConnectionFactory.newPrivateConnection()).createStatement();
+    }
+
+
+    /**
+     *
+     * @see <a href="https://ecosystem.atlassian.net/browse/AO-364">AO-364</a>
+     * @throws Exception because it's a test :P
+     */
+    @Test
+    public void testIfExceptionsAreHandledCorrectly() throws Exception
+    {
+        final Connection connectionMock = mock(Connection.class);
+        final ConnectionHandler.Closeable closeableMock = mock(ConnectionHandler.Closeable.class);
+
+        final SQLException expectedException = new SQLException("An exception that should be re-thrown by proxy object");
+        when(connectionMock.prepareStatement(any(String.class))).thenThrow(expectedException);
+        final Connection connectionHandler = ConnectionHandler.newInstance(connectionMock, closeableMock);
+        exception.expect(SQLException.class);
+        exception.expectMessage(CoreMatchers.equalTo("An exception that should be re-thrown by proxy object"));
+        connectionHandler.prepareStatement("this call should throw SQLException");
     }
 
 }
