4b9bf24aea4dd8cbdc3c6f93fabe6cc071b5ad80
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2012-09-27 17:09:39 +0200

    JRA-25363: improved error reporting

--- activeobjects-spi/src/main/java/com/atlassian/activeobjects/spi/ActiveObjectsImportExportException.java
+++ activeobjects-spi/src/main/java/com/atlassian/activeobjects/spi/ActiveObjectsImportExportException.java
@@ -1,28 +1,31 @@
 package com.atlassian.activeobjects.spi;
 
-
-import static com.google.common.base.Preconditions.*;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class ActiveObjectsImportExportException extends ImportExportException
 {
     private final PluginInformation pluginInformation;
+    private final String tableName;
 
-    public ActiveObjectsImportExportException(PluginInformation pluginInformation, String message)
+    public ActiveObjectsImportExportException(String tableName, PluginInformation pluginInformation, String message)
     {
         super(message);
         this.pluginInformation = checkNotNull(pluginInformation);
+        this.tableName = tableName;
     }
 
-    public ActiveObjectsImportExportException(PluginInformation pluginInformation, Throwable t)
+    public ActiveObjectsImportExportException(String tableName, PluginInformation pluginInformation, Throwable t)
     {
         super(t);
         this.pluginInformation = checkNotNull(pluginInformation);
+        this.tableName = tableName;
     }
 
-    public ActiveObjectsImportExportException(PluginInformation pluginInformation, String message, Throwable t)
+    public ActiveObjectsImportExportException(String tableName, PluginInformation pluginInformation, String message, Throwable t)
     {
         super(message, t);
         this.pluginInformation = checkNotNull(pluginInformation);
+        this.tableName = tableName;
     }
 
     public PluginInformation getPluginInformation()
@@ -30,9 +33,15 @@ public final class ActiveObjectsImportExportException extends ImportExportExcept
         return pluginInformation;
     }
 
+    public String getTableName() {
+        return tableName;
+    }
+
     @Override
     public String getMessage()
     {
-        return "There was an error during import/export with " + pluginInformation + ":" + super.getMessage();
+        return "There was an error during import/export with " + pluginInformation
+                + (tableName != null ? " (table " + tableName + ")" : "")
+                + ":" + super.getMessage();
     }
 }
