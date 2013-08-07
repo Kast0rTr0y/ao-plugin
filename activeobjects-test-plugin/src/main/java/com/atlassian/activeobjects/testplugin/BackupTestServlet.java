40913345f60114442e4c746cf5d9a1a5c71a35d0
Steve Lancashire <slancashire@atlassian.com>
2013-08-07 14:59:52 +1000

    AO-330 added verification that ao can be used during initialization

--- activeobjects-test-plugin/src/main/java/com/atlassian/activeobjects/testplugin/BackupTestServlet.java
+++ activeobjects-test-plugin/src/main/java/com/atlassian/activeobjects/testplugin/BackupTestServlet.java
@@ -4,21 +4,27 @@ import com.atlassian.activeobjects.external.ActiveObjects;
 import com.atlassian.activeobjects.spi.Backup;
 import com.atlassian.activeobjects.spi.NullBackupProgressMonitor;
 import com.atlassian.activeobjects.spi.NullRestoreProgressMonitor;
+import com.atlassian.activeobjects.test.model.Author;
 import com.atlassian.activeobjects.test.model.Model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 
-import static com.google.common.base.Preconditions.*;
+import static com.google.common.base.Preconditions.checkNotNull;
 
-public class BackupTestServlet extends HttpServlet
+public class BackupTestServlet extends HttpServlet implements InitializingBean
 {
     public static final String CREATE = "create";
     public static final String BACKUP = "backup";
@@ -27,6 +33,7 @@ public class BackupTestServlet extends HttpServlet
     private final ActiveObjects ao;
     private final Backup backup;
 
+    private static Logger log = LoggerFactory.getLogger(BackupTestServlet.class);
 
     public BackupTestServlet(ActiveObjects ao, Backup backup)
     {
@@ -79,5 +86,33 @@ public class BackupTestServlet extends HttpServlet
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
         new Model(ao).emptyDatabase();
+    }
+    
+    @Override
+    public void afterPropertiesSet() throws Exception
+    {
+        //accessing active objects during plugin initialization used to blow up : AO-330
+        Model model = new Model(ao);
+        try
+        {
+            
+            model.migrateEntities();
+            model.createData();
+        }
+        catch(Exception ex)
+        {
+            throw new RuntimeException("Failed to access active objects during plugin initialization", ex);
+        }
+        
+        // can't use checkXXX methods as junit is not bundled in the plugin
+        int authorCount = ao.count(Author.class);
+        if (authorCount <= 0)
+        {
+            throw new IllegalStateException("Should have found some data "+authorCount);
+        }
+        
+        model.emptyDatabase();
+        
+        log.info("succeeded in accessing active objects whilst initializing a plugin");
     }
 }
