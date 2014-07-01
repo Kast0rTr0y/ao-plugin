5365115b40847655d2d351d4fdb108d6c15a0b3d
Alexander Courtis <acourtis@atlassian.com>
2014-07-01 13:52:16 +1000

    AO-530 clarify documentation of HotRestartEvent

--- activeobjects-spi/src/main/java/com/atlassian/activeobjects/spi/HotRestartEvent.java
+++ activeobjects-spi/src/main/java/com/atlassian/activeobjects/spi/HotRestartEvent.java
@@ -3,8 +3,8 @@ package com.atlassian.activeobjects.spi;
 /**
  * <p>This is the event that products can publish via their own EventPublisher to tell AO
  * to hot restart itself.</p>
- * <p>The effect of this will mainly be that AO will release all ActiveObject instances and flush all caches they
- * contain.</p>
+ * <p>The effect of this will mainly be that AO will reconnect to the database and run the (idempotent) upgrade tasks
+ * again.</p>
  */
 public final class HotRestartEvent
 {
