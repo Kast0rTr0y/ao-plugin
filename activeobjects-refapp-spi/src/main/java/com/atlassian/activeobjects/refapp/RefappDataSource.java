343977864ab68babf4005e9001148c0674105bfd
Alexander Courtis <acourtis@atlassian.com>
2014-08-19 15:00:25 +1000

    AO-546 refapp-spi uses a java.sql.Connection provided by refapp, instead of creating its own

--- activeobjects-refapp-spi/src/main/java/com/atlassian/activeobjects/refapp/RefappDataSource.java
+++ activeobjects-refapp-spi/src/main/java/com/atlassian/activeobjects/refapp/RefappDataSource.java
@@ -1,132 +1,75 @@
 package com.atlassian.activeobjects.refapp;
 
-import com.atlassian.sal.api.ApplicationProperties;
-import org.hsqldb.jdbc.jdbcDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.atlassian.refapp.api.ConnectionProvider;
 
-import javax.sql.DataSource;
-import java.io.File;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
+import javax.sql.DataSource;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-/**
- *
- */
 public final class RefappDataSource implements DataSource
 {
-    private static final String DEFAULT_BASE_DIR = "data/plugins/activeobjects";
-    private static final String DEFAULT_USERNAME = "sa";
-    private static final String DEFAULT_PASSWORD = "";
+    private final ConnectionProvider connectionProvider;
 
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
-
-    private final DataSource delegate;
-
-    public RefappDataSource(ApplicationProperties applicationProperties)
+    public RefappDataSource(final ConnectionProvider connectionProvider)
     {
-        checkNotNull(applicationProperties);
-        delegate = createDelegate(checkHomeDirectory(applicationProperties.getHomeDirectory()));
-    }
-
-    private DataSource createDelegate(final File homeDirectory)
-    {
-        final jdbcDataSource dataSource = new jdbcDataSource();
-        dataSource.setDatabase(createJdbcUrl(homeDirectory));
-        dataSource.setUser(DEFAULT_USERNAME);
-        dataSource.setPassword(DEFAULT_PASSWORD);
-        return dataSource;
-    }
-
-    private String createJdbcUrl(final File homeDirectory)
-    {
-        final File dbDirectory = new File(homeDirectory, DEFAULT_BASE_DIR);
-        if (dbDirectory.exists() && dbDirectory.isFile())
-        {
-            throw new RuntimeException("Database directory already exists, but is a file, at <" + dbDirectory.getPath() + ">");
-        }
-
-        if (!dbDirectory.exists() && !dbDirectory.mkdirs())
-        {
-            throw new RuntimeException("Could not create directory for database at <" + dbDirectory.getPath() + ">");
-        }
-
-        log.debug("ActiveObjects databases directory {} initialized", dbDirectory.getAbsolutePath());
-
-        return new StringBuilder("jdbc:hsqldb:")
-                .append(dbDirectory.getAbsolutePath())
-                .append(";hsqldb.default_table_type=cached").toString();
+        this.connectionProvider = checkNotNull(connectionProvider);
     }
 
     @Override
     public Connection getConnection() throws SQLException
     {
-        return delegate.getConnection();
+        return connectionProvider.connection();
     }
 
     @Override
     public Connection getConnection(String username, String password) throws SQLException
     {
-        return delegate.getConnection(username, password);
+        return connectionProvider.connection();
     }
 
     @Override
     public PrintWriter getLogWriter() throws SQLException
     {
-        return delegate.getLogWriter();
+        throw new UnsupportedOperationException("Not implemented");
     }
 
     @Override
-    public void setLogWriter(PrintWriter out) throws SQLException
+    public void setLogWriter(final PrintWriter out) throws SQLException
     {
-        delegate.setLogWriter(out);
+        throw new UnsupportedOperationException("Not implemented");
     }
 
     @Override
-    public void setLoginTimeout(int seconds) throws SQLException
+    public void setLoginTimeout(final int seconds) throws SQLException
     {
-        delegate.setLoginTimeout(seconds);
+        throw new UnsupportedOperationException("Not implemented");
     }
 
     @Override
     public int getLoginTimeout() throws SQLException
     {
-        return delegate.getLoginTimeout();
+        throw new UnsupportedOperationException("Not implemented");
     }
 
     @Override
-    public <T> T unwrap(Class<T> tClass) throws SQLException
+    public <T> T unwrap(final Class<T> iface) throws SQLException
     {
-        return delegate.unwrap(tClass);
+        throw new UnsupportedOperationException("Not implemented");
     }
 
     @Override
-    public boolean isWrapperFor(Class<?> aClass) throws SQLException
+    public boolean isWrapperFor(final Class<?> iface) throws SQLException
     {
-        return delegate.isWrapperFor(aClass);
+        throw new UnsupportedOperationException("Not implemented");
     }
 
-    // @Override Java 7 only
+    // @Override // Java 7 only
     public java.util.logging.Logger getParentLogger() throws SQLFeatureNotSupportedException
     {
-        throw new SQLFeatureNotSupportedException();
-    }
-
-    private static File checkHomeDirectory(final File homeDirectory)
-    {
-        if (homeDirectory == null)
-        {
-            throw new RuntimeException("no home directory defined by product");
-        }
-
-        if (!homeDirectory.exists() || !homeDirectory.isDirectory())
-        {
-            throw new RuntimeException("Couldn't find product home directory at '" + homeDirectory.getAbsolutePath() + "'");
-        }
-        return homeDirectory;
+        throw new UnsupportedOperationException("Not implemented");
     }
 }
