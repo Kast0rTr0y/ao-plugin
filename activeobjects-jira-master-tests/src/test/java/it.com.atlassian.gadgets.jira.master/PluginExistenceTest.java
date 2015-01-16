9185339b1122e869746bc77fe3e5d4cd8791e05e
Alexander Courtis <acourtis@atlassian.com>
2015-01-16 16:22:53 +1100

    Revert "AO-370: removed module to test AO with JIRA master"
    This reverts commit 2bfa70004e7139c9a7953485a56a543ec36969b9.
    
    Conflicts:
    	activeobjects-jira-master-tests/pom.xml

---
+++ activeobjects-jira-master-tests/src/test/java/it.com.atlassian.gadgets.jira.master/PluginExistenceTest.java
@@ -0,0 +1,22 @@
+package it.com.atlassian.gadgets.jira.master;
+
+import com.atlassian.jira.nimblefunctests.framework.NimbleFuncTestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @since v3.2
+ */
+public class PluginExistenceTest extends NimbleFuncTestCase {
+
+	public static final String PLUGIN_KEY = "com.atlassian.activeobjects.activeobjects-plugin";
+
+	@Test
+	public void testIfPluginIsInstalledAndEnabled() throws Exception
+	{
+		assertTrue("Plugin is not installed", administration.plugins().isPluginInstalled(PLUGIN_KEY));
+		assertTrue("Plugin is not enabled", administration.plugins().isPluginEnabled(PLUGIN_KEY));
+	}
+
+}
