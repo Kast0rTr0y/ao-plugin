532d63e7edc1c2a2b73670a1254b689f12bb3c1c
Alexander Courtis <acourtis@atlassian.com>
2015-05-22 13:12:01 +1000

    AO-648 don't complain when a TenantAwareActiveObjects receives the same <ao> configuration twice

--- activeobjects-core/src/main/java/com/atlassian/activeobjects/external/NoDataSourceException.java
+++ activeobjects-core/src/main/java/com/atlassian/activeobjects/external/NoDataSourceException.java
@@ -7,6 +7,7 @@ public final class NoDataSourceException extends IllegalStateException
         super("No data source is available, indicating that there is no tenant available to the "
                 + "application. Your plugin is probably performing tenant specific operations too eagerly.\n"
                 + "com.atlassian.activeobjects.external.ActiveObjectsModuleMetaData contains utility methods that "
-                + "can assist you in this situation.");
+                + "can assist you in this situation.\n"
+                + "isDataSourcePresent will safely indicate the availability of the data source.");
     }
 }
