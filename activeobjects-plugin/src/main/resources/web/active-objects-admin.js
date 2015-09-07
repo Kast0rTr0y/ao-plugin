67a3be3ea1ed401fda928ee35e726a77ae9e4796
Dariusz Szuksztul <dszuksztul@atlassian.com>
2015-09-07 14:04:02 +0200

    JDEV-34309 - make use of amd modules

--- activeobjects-plugin/src/main/resources/web/active-objects-admin.js
+++ activeobjects-plugin/src/main/resources/web/active-objects-admin.js
@@ -1,11 +1,15 @@
 (function () {
-    var $ = AJS.$;
+    var localRequire = typeof require === 'function' ?
+        require :
+        function (deps, callback) {
+            callback(AJS.$);
+        };
 
-    $(document).ready(function ()
-    {
-       $(".ao-plugin").click(function()
-       {
-           $(this).toggleClass("ao-plugin-selected");
-       });
+    localRequire(['jquery'], function ($) {
+        $(document).ready(function () {
+            $(".ao-plugin").click(function () {
+                $(this).toggleClass("ao-plugin-selected");
+            });
+        });
     });
 })();
