af930ab1572f5cd7afc29aff70bf306aab62c200
Nick Clarke <nclarke@atlassian.com>
2015-10-09 14:32:02 +1100

    Copy DispatcherServlet change from embedded-crowd-admin to make spring4 webmvc work in Confluence

--- activeobjects-plugin/src/main/java/com/atlassian/plugin/web/springmvc/DispatcherServlet.java
+++ activeobjects-plugin/src/main/java/com/atlassian/plugin/web/springmvc/DispatcherServlet.java
@@ -15,25 +15,50 @@ public final class DispatcherServlet extends org.springframework.web.servlet.Dis
         setPublishContext(false);
     }
 
+    /**
+     * In Spring 4 we have to override
+     * {@link #initWebApplicationContext()} instead of {@link #findWebApplicationContext()},
+     * because initWebApplicationContext has changed from Spring 2.5.6 and now calls
+     * {@link org.springframework.web.context.support.WebApplicationContextUtils#getWebApplicationContext(
+     * javax.servlet.ServletContext)} in Confluence this returns something referencing confluence's internal copy of
+     * spring-web (which got there from
+     * {@link org.springframework.web.context.ContextLoader#initWebApplicationContext(javax.servlet.ServletContext)})
+     * - that gives a classcast because this plugin is using the plugin framework's spring.
+     * <p>
+     * This override is safe for our use case with the Spring 4.1.4 implementation, but may be fragile with future upgrades.
+     *
+     * Possible alternative solution: define a servlet-filter in atlassian-plugin.xml and have it override/hide the
+     * {@link WebApplicationContext#ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE} attribute.
+     */
     @Override
-    protected WebApplicationContext findWebApplicationContext() {
+    protected WebApplicationContext initWebApplicationContext()
+    {
         // use the plugin Spring context as the parent for a new web application context
-        final XmlWebApplicationContext context = new XmlWebApplicationContext() {
+        XmlWebApplicationContext context = new XmlWebApplicationContext() {
+
             @Override
-            protected void initBeanDefinitionReader(final XmlBeanDefinitionReader beanDefinitionReader) {
+            protected void initBeanDefinitionReader(final XmlBeanDefinitionReader beanDefinitionReader)
+            {
                 beanDefinitionReader.setValidationMode(XmlBeanDefinitionReader.VALIDATION_NONE);
                 super.initBeanDefinitionReader(beanDefinitionReader);
             }
         };
-        context.setId("ECWebApplicationContext");
+        context.setId("AOWebApplicationContext");
         context.setParent(pluginSpringContext);
         context.setConfigLocation(getContextConfigLocation());
         context.setServletContext(getServletContext());
         context.refresh();
+
+        // Required because the overridden super method would usually call this
+        onRefresh(context);
+
         return context;
     }
 
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        super.setApplicationContext(applicationContext);
         this.pluginSpringContext = applicationContext;
     }
 }
